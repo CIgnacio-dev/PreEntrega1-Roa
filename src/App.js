d0d522c897866e1772ee743e4644192817266b35
Carlos Ignacio Roa <60223426+CIgnacio-dev@users.noreply.github.com>
2023-06-16 19:33:03 -0400

    Add files via upload
    Creación de Routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from '../src/Components/About';
+import Contact from './Components/Contact';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ItemListContainer from './Components/ItemListContainer';
+import ItemList from './Components/ItemList';
+import ItemDetails from './Components/ItemDetails';
 
 
-function App() {
+
+let App= () => {
+  let items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
   return (
-    <div className="App">
-      <NavBar />
-      <ItemListContainer greeting="¡Bienvenido a las mejores ofertas para conocer el mundo!" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemList items={items} />} />
+          <Route path="/items/:id" element={<ItemDetails />} />
+          <Route path="./Components/About" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="./Components/items" element={<ItemList items={items} />} />
+          
+        </Routes>
+       
+      </div>
+    </BrowserRouter>
+    
   );
+  
 }
 
 export default App;
